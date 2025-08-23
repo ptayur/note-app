3f0b200b4f90e160c9232872bd3514c3abc449e5
ptayur <yurii.ptshnk@gmail.com>
2025-08-23 14:19:40 +0300

    refactor(notes): replace default User model with CustomUser

--- note_app/models.py
+++ note_app/models.py
@@ -1,11 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
+from accounts.models import CustomUser
 
 
 class Note(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
