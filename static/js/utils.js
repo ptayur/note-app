3e4c5dfb4fe1a3ec382372245d89ad35aa502266
ptayur <yurii.ptshnk@gmail.com>
2025-08-26 18:50:21 +0300

    fix(utils): refactor apiFetch to jwtRequest with token handling and refresh logic

--- static/js/utils.js
+++ static/js/utils.js
@@ -2,17 +2,42 @@
 // API Requests
 //
 
-export async function apiFetch(url, options) {
+export async function jwtRequest(url, options = {}, { auth = true } = {}) {
     try {
-        const response = await fetch(url, options || {});
+        let headers = { ...options.headers };
+        if (auth) {
+            headers["Authorization"] = "Bearer " + localStorage.getItem("access_token");
+        }
+        let response = await fetch(url, {
+            ...options,
+            headers,
+        });
 
-        const text = await response.text();
-        const data = text ? JSON.parse(text) : null;  
+        if (auth && response.status === 403) {
+            const refreshResponse = await fetch("/auth/refresh/", {
+                method: "POST",
+            });
 
+            if (refreshResponse.ok) {
+                const refreshData = await refreshResponse.json();
+                localStorage.setItem("access_token", refreshData.access_token)
+
+                response = await fetch(url, {
+                    ...options,
+                    headers: {...options.headers,
+                        "Authorization": "Bearer " + localStorage.getItem("access_token")
+                    }
+                });
+            } else {
+                window.location.replace("/auth/login/");
+            }
+        }
+
+        const data = await response.json();
         return {
             ok: response.ok,
             status: response.status,
-            data
+            data,
         };
 
     } catch (error) {
