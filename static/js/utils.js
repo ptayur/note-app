4e7d6798f26f56b6ad1a18403be0808e003e9342
ptayur <yurii.ptshnk@gmail.com>
2025-09-07 16:51:16 +0300

    chore: move utils.js to project root

--- static/js/utils.js
+++ static/js/utils.js
@@ -20,13 +20,13 @@ export async function jwtRequest(url, options = {}, { auth = true } = {}) {
         });
 
         if (auth && response.status === 401) {
-            const refreshResponse = await fetch("/api/refresh/", {
+            const refreshResponse = await fetch("/api/users/refresh/", {
                 method: "POST",
             });
 
             if (refreshResponse.ok) {
                 const refreshData = await refreshResponse.json();
-                localStorage.setItem("access_token", refreshData.access_token)
+                localStorage.setItem("access_token", refreshData.data.access_token)
 
                 response = await fetch(url, {
                     ...options,
@@ -34,8 +34,6 @@ export async function jwtRequest(url, options = {}, { auth = true } = {}) {
                         "Authorization": "Bearer " + localStorage.getItem("access_token")
                     }
                 });
-            } else {
-                window.location.replace("/auth/");
             }
         }
 
@@ -53,4 +51,20 @@ export async function jwtRequest(url, options = {}, { auth = true } = {}) {
     } catch (error) {
         return {ok: false, status: 0, data: {error: error.message}}
     }
+}
+
+export function generateSVG({ svgAttrs = {} , pathAttrs = {}, className = "icon" } = {}) {
+    const iconSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const iconPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
+    for (const [key, value] of Object.entries(svgAttrs)) {
+        iconSVG.setAttribute(key, value);
+    };
+    iconSVG.classList.add(className);
+
+    for (const [key, value] of Object.entries(pathAttrs)) {
+        iconPath.setAttribute(key, value);
+    };
+    iconSVG.appendChild(iconPath);
+    return iconSVG;
 }
