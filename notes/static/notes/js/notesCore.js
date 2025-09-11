224d637f6e1219d6b32a22f26605dba65148f6db
ptayur <yurii.ptshnk@gmail.com>
2025-09-11 12:52:28 +0300

    refactor: update updateNote for proper request

--- notes/static/notes/js/notesCore.js
+++ notes/static/notes/js/notesCore.js
@@ -23,16 +23,50 @@ export async function readNotes() {
     });
 }
 
-export async function updateNote(id, data) {
-    return await jwtRequest(`/api/notes/${id}/`, {
+export async function updateNote(note, data) {
+    const noteId = note.dataset.id;
+    const noteTitle = note.querySelector(".note-title");
+    const noteContent = note.querySelector(".note-content");
+
+    // Build payload with only changed field
+    const payload = {};
+    if (data.title !== noteTitle.textContent) {
+        payload.title = data.title;
+    }
+    if (data.content !== noteContent.textContent) {
+        payload.content = data.content;
+    }
+
+    // Return early if nothing changed
+    if (Object.keys(payload).length === 0) {
+        return { ok: false, status: 0, data: {"detail": "No changes provided."} };
+    }
+
+    const response = await jwtRequest(`/api/notes/${noteId}/`, {
         method: "PATCH",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data)
+        body: JSON.stringify(payload)
     });
+    if (response.ok) {
+        // Change note title and content on success
+        noteTitle.textContent = response.data.title;
+        noteContent.textContent = response.data.content;
+    } else {
+        // Display error modal box
+        console.log("Edit response not ok");
+    }
 }
 
 export async function deleteNote(id) {
     return await jwtRequest(`/api/notes/${id}/`, {
         method: "DELETE"
+    });
+}
+
+export async function shareNote(data) {
+    return await jwtRequest("/api/notes/shares", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
     });
 }
