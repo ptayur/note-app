113664ba9da5e2ad637457a78d317c3e2a845b46
ptayur <yurii.ptshnk@gmail.com>
2025-09-14 20:04:01 +0300

    chore: remove unused function

--- static/js/utils.js
+++ static/js/utils.js
@@ -62,19 +62,3 @@ export async function jwtRequest(url, options = {}, { auth = true } = {}) {
         return {ok: false, status: 0, data: {error: error.message}}
     }
 }
-
-export function generateSVG({ svgAttrs = {} , pathAttrs = {}, className = "icon" } = {}) {
-    const iconSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    const iconPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
-
-    for (const [key, value] of Object.entries(svgAttrs)) {
-        iconSVG.setAttribute(key, value);
-    };
-    iconSVG.classList.add(className);
-
-    for (const [key, value] of Object.entries(pathAttrs)) {
-        iconPath.setAttribute(key, value);
-    };
-    iconSVG.appendChild(iconPath);
-    return iconSVG;
-}
