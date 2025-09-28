c98e029dcaa65828bf770e0a2ebc9ca20dd2b52a
ptayur <yurii.ptshnk@gmail.com>
2025-09-28 16:08:06 +0300

    fix: convert Permission object into str

--- notes/serializers/share_serializer.py
+++ notes/serializers/share_serializer.py
@@ -7,21 +7,23 @@ class ShareSerializer(serializers.HyperlinkedModelSerializer):
     """Shares serializer for endpoint"""
 
     user = serializers.SlugRelatedField(queryset=CustomUser.objects.all(), slug_field="username")
-    note = serializers.HyperlinkedRelatedField(
-        queryset=Note.objects.all(), view_name="note-detail", lookup_field="pk"
-    )
-    permissions = serializers.SlugRelatedField(
-        queryset=Permissions.objects.all(), many=True, slug_field="code"
-    )
+
+    # Send URL in response
+    note = serializers.HyperlinkedRelatedField(view_name="note-detail", lookup_field="pk", read_only=True)
+    # Accept int as note identifier
+    note_id = serializers.PrimaryKeyRelatedField(source="note", queryset=Note.objects.all(), write_only=True)
+
+    permissions = serializers.SlugRelatedField(queryset=Permissions.objects.all(), many=True, slug_field="code")
 
     class Meta:
         model = Share
-        fields = ["url", "id", "user", "note", "permissions"]
+        fields = ["url", "id", "user", "note", "note_id", "permissions"]
 
     def create(self, validated_data):
         perms = validated_data.pop("permissions", [])
         share = super().create(validated_data)
         if perms:
+            perms = [perm.code for perm in perms]
             share.set_perms(perms)
         return share
 
@@ -29,6 +31,7 @@ class ShareSerializer(serializers.HyperlinkedModelSerializer):
         perms = validated_data.pop("permissions", None)
         instance = super().update(instance, validated_data)
         if perms is not None:
+            perms = [perm.code for perm in perms]
             instance.set_perms(perms)
         return instance
 
