8343f57677ef41b5a2f061565784d2be92b476e2
ptayur <yurii.ptshnk@gmail.com>
2025-10-11 21:12:40 +0300

    feat: implement batch factories

--- tests/factories.py
+++ tests/factories.py
@@ -1,16 +1,27 @@
 import pytest
+from collections.abc import Callable
+from typing import Any
 from model_bakery import baker
 from accounts.models import CustomUser
 
 
 @pytest.fixture
-def user_factory():
-    def make_users(n: int = 1, **kwargs) -> CustomUser | list[CustomUser]:
-        users = baker.make(
+def user_factory() -> Callable[..., CustomUser]:
+    def make_user(**kwargs: Any) -> CustomUser:
+        return baker.make(
             CustomUser,
-            _quantity=n,
             **kwargs,
         )
-        return users[0] if n == 1 else users
+
+    return make_user
+
+
+@pytest.fixture
+def user_factory_batch() -> Callable[[int], list[CustomUser]]:
+    def make_users(n: int) -> list[CustomUser]:
+        return baker.make(
+            CustomUser,
+            _quantity=n,
+        )
 
     return make_users
