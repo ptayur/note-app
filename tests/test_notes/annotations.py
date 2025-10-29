d0081ae73b2433b6c1a94cba1a1403195b397d15
ptayur <yurii.ptshnk@gmail.com>
2025-10-29 12:52:54 +0200

    test: improve annotations

--- tests/test_notes/annotations.py
+++ tests/test_notes/annotations.py
@@ -1,6 +1,6 @@
 from tests.annotations import UserFactory, UserFactoryBatch, Authenticate
 from collections.abc import Callable
-from typing import TypeAlias, TypedDict
+from typing import TypeAlias, TypedDict, Protocol
 from notes.models import Note, Share
 from accounts.models import CustomUser
 
@@ -8,6 +8,14 @@ NoteFactory: TypeAlias = Callable[..., Note]
 NoteFactoryBatch: TypeAlias = Callable[[int], list[Share]]
 ShareFactory: TypeAlias = Callable[..., Share]
 ShareFactoryBatch: TypeAlias = Callable[[int], list[Share]]
+
+
+class GetNotesUrl(Protocol):
+    def __call__(self, pk: int | None = None) -> str: ...
+
+
+class GetSharesUrl(Protocol):
+    def __call__(self, note_pk: int | None = None, pk: int | None = None) -> str: ...
 
 
 class PrepareNotes(TypedDict):
