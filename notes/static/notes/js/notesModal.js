c909eef214802ac9648d0356874a5c5b0a46b09b
ptayur <yurii.ptshnk@gmail.com>
2025-11-03 11:56:25 +0200

    refactor: enhance modal styling

--- notes/static/notes/js/notesModal.js
+++ notes/static/notes/js/notesModal.js
@@ -5,126 +5,126 @@
 import { ModalManager } from "/static/components/modal/modalManager.js";
 
 export function noteDetailsModal(noteData) {
-    const modal = new ModalManager();
+  const modal = new ModalManager();
 
-    // Header structure
-    const h5 = document.createElement("h5");
-    h5.textContent = noteData.title;
-    const closeBtn = document.createElement("button");
-    closeBtn.textContent = "Close";
-    closeBtn.classList.add("generic-button");
+  // Header structure
+  const p = document.createElement("p");
+  p.textContent = noteData.title;
+  const closeBtn = document.createElement("button");
+  closeBtn.textContent = "Close";
+  closeBtn.classList.add("generic-button");
 
-    closeBtn.addEventListener("click", () => modal.close());
+  closeBtn.addEventListener("click", () => modal.close());
 
-    // Main structure
-    const fields = { 
-        user: "Owner",
-        created_at: "Creation time",
-        updated_at: "Last updated" 
-    };
-    const ul = document.createElement("ul");
-    ul.classList.add("modal__list");
+  // Main structure
+  const fields = {
+    owner: "Owner",
+    created_at: "Creation time",
+    updated_at: "Last updated",
+  };
+  const ul = document.createElement("ul");
+  ul.classList.add("modal__list");
 
-    Object.entries(fields).forEach(([key, label]) => {
-        const li = document.createElement("li");
-        
-        // Label section
-        const fieldLabel = document.createElement("div");
-        fieldLabel.classList.add("list__label");
+  Object.entries(fields).forEach(([key, label]) => {
+    const li = document.createElement("li");
 
-        const pLabel = document.createElement("p");
-        pLabel.textContent = `${label}: `
+    // Label section
+    const fieldLabel = document.createElement("div");
+    fieldLabel.classList.add("list__label");
 
-        fieldLabel.appendChild(pLabel);
+    const pLabel = document.createElement("p");
+    pLabel.textContent = `${label}: `;
 
-        // Key section
-        const fieldKey = document.createElement("div");
-        fieldKey.classList.add("list__key");
+    fieldLabel.appendChild(pLabel);
 
-        const pKey = document.createElement("p");
-        pKey.textContent = `${noteData[key]}`;
+    // Key section
+    const fieldKey = document.createElement("div");
+    fieldKey.classList.add("list__key");
 
-        fieldKey.appendChild(pKey);
+    const pKey = document.createElement("p");
+    pKey.textContent = `${noteData[key]}`;
 
-        li.appendChild(fieldLabel);
-        li.appendChild(fieldKey);
-        ul.appendChild(li);
-    });
+    fieldKey.appendChild(pKey);
 
-    // Footer structure
+    li.appendChild(fieldLabel);
+    li.appendChild(fieldKey);
+    ul.appendChild(li);
+  });
 
-    const okBtn = document.createElement("button");
-    okBtn.textContent = "Ok";
-    okBtn.classList.add("generic-button");
+  // Footer structure
 
-    okBtn.addEventListener("click", () => modal.close());
+  const okBtn = document.createElement("button");
+  okBtn.textContent = "Ok";
+  okBtn.classList.add("generic-button");
 
-    //Modal setup
-    modal.setContent({
-        header: [h5, closeBtn],
-        main: ul,
-        footer: okBtn
-    });
+  okBtn.addEventListener("click", () => modal.close());
 
-    modal.setClass({
-        modal: "modal--generic",
-        modalWindow: "modal__window--generic"
-    });
+  //Modal setup
+  modal.setContent({
+    header: [p, closeBtn],
+    main: ul,
+    footer: okBtn,
+  });
 
-    modal.show();
+  modal.setClass({
+    modal: "modal--generic",
+    modalWindow: "modal__window--generic",
+  });
+
+  modal.show();
 }
 
 export function confirmDeleteModal(noteTitle) {
-    const modal = new ModalManager();
+  const modal = new ModalManager();
 
-    // Header structure
-    const pTitle = document.createElement("p");
-    pTitle.textContent = "Confirm deletion";
+  // Header structure
+  const pTitle = document.createElement("p");
+  pTitle.textContent = "Confirm deletion";
 
-    const closeBtn = document.createElement("button");
-    closeBtn.textContent = "Close";
-    closeBtn.classList.add("generic-button");
-    // Main structure
-    
-    const pMessage = document.createElement("p");
-    pMessage.textContent = `Are you sure you want to delete note "${noteTitle}"?`;
+  const closeBtn = document.createElement("button");
+  closeBtn.textContent = "Close";
+  closeBtn.classList.add("generic-button");
+  // Main structure
 
-    // Footer structure
+  const pMessage = document.createElement("p");
+  pMessage.textContent = `Are you sure you want to delete note "${noteTitle}"?`;
 
-    const cancelBtn = document.createElement("button");
-    cancelBtn.textContent = "Cancel";
-    cancelBtn.classList.add("generic-button");
+  // Footer structure
 
-    const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
-    deleteBtn.classList.add("modal__button--delete");
+  const cancelBtn = document.createElement("button");
+  cancelBtn.textContent = "Cancel";
+  cancelBtn.classList.add("generic-button");
 
-    //Modal setup
-    modal.setContent({
-        header: [pTitle, closeBtn],
-        main: pMessage,
-        footer: [cancelBtn, deleteBtn]
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.classList.add("modal__button--delete", "generic-button");
+
+  //Modal setup
+  modal.setContent({
+    header: [pTitle, closeBtn],
+    main: pMessage,
+    footer: [cancelBtn, deleteBtn],
+  });
+
+  modal.setClass({
+    modal: "modal--generic",
+    modalWindow: "modal__window--generic",
+  });
+
+  modal.show();
+
+  return new Promise((resolve) => {
+    closeBtn.addEventListener("click", () => {
+      modal.close();
+      resolve(false);
     });
-
-    modal.setClass({
-        modal: "modal--confirm",
-        modalWindow: "modal__window--confirm"
+    cancelBtn.addEventListener("click", () => {
+      modal.close();
+      resolve(false);
     });
-
-    modal.show();
-
-    return new Promise(resolve => {
-        closeBtn.addEventListener("click", () => {
-            modal.close();
-            resolve(false);
-        });
-        cancelBtn.addEventListener("click", () => {
-            modal.close();
-            resolve(false);
-        });
-        deleteBtn.addEventListener("click", () => {
-            modal.close();
-            resolve(true);
-        });
-    })
+    deleteBtn.addEventListener("click", () => {
+      modal.close();
+      resolve(true);
+    });
+  });
 }
