0fe7afec1808bf1eca7ff3a1b01e32460a385ff5
ptayur <yurii.ptshnk@gmail.com>
2025-11-06 12:24:17 +0200

    fix: error handling

--- notes/static/notes/js/notesAPI.js
+++ notes/static/notes/js/notesAPI.js
@@ -1,24 +1,15 @@
 import { jwtRequest, AppError } from "/static/js/utils.js";
 
-async function checkResponse(response, errorTitle) {
-  if (!response.ok) {
-    throw new AppError(errorTitle, response.data?.error || `HTTP error: ${response.status}`);
-  }
-  return response.data;
-}
-
 // ------------------
 // Notes API Requests
 // ------------------
 
 export async function createNote(data) {
-  const response = await jwtRequest("/api/v1/notes/", {
+  return await jwtRequest("/api/v1/notes/", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   });
-
-  return checkResponse(response, "Create request error");
 }
 
 export async function getNoteList(filterParams = null) {
@@ -26,38 +17,30 @@ export async function getNoteList(filterParams = null) {
   if (filterParams) {
     url += `?${filterParams}`;
   }
-  const response = await jwtRequest(url, {
+  return await jwtRequest(url, {
     method: "GET",
     headers: { "Content-Type": "application/json" },
   });
-
-  return await checkResponse(response, "Get notes request error");
 }
 
 export async function getNoteDetails(noteId) {
-  const response = await jwtRequest(`/api/v1/notes/${noteId}/`, {
+  return await jwtRequest(`/api/v1/notes/${noteId}/`, {
     method: "GET",
   });
-
-  return await checkResponse(response, "Get note request error");
 }
 
 export async function updateNote(noteId, payload) {
-  const response = await jwtRequest(`/api/v1/notes/${noteId}/`, {
+  return await jwtRequest(`/api/v1/notes/${noteId}/`, {
     method: "PATCH",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(payload),
   });
-
-  return await checkResponse(response, "Update request error");
 }
 
 export async function deleteNote(noteId) {
-  const response = await jwtRequest(`/api/v1/notes/${noteId}/`, {
+  return await jwtRequest(`/api/v1/notes/${noteId}/`, {
     method: "DELETE",
   });
-
-  return await checkResponse(response, "Delete request error");
 }
 
 // -------------------
@@ -65,39 +48,31 @@ export async function deleteNote(noteId) {
 // -------------------
 
 export async function createNoteShare(noteId, data) {
-  const response = await jwtRequest(`/api/v1/notes/${noteId}/shares/`, {
+  return await jwtRequest(`/api/v1/notes/${noteId}/shares/`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   });
-
-  return await checkResponse(response);
 }
 
 export async function getNoteShares(noteId) {
-  const response = await jwtRequest(`/api/v1/notes/${noteId}/shares/`, {
+  return await jwtRequest(`/api/v1/notes/${noteId}/shares/`, {
     method: "GET",
     headers: { "Content-Type": "application/json" },
   });
-
-  return await checkResponse(response);
 }
 
 export async function updateNoteShare(noteId, shareId, data) {
-  const response = await jwtRequest(`/api/v1/notes/${noteId}/shares/${shareId}/`, {
+  return await jwtRequest(`/api/v1/notes/${noteId}/shares/${shareId}/`, {
     method: "PATCH",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   });
-
-  return await checkResponse(response);
 }
 
 export async function deleteNoteShare(noteId, shareId) {
-  const response = await jwtRequest(`/api/v1/notes/${noteId}/shares/${shareId}/`, {
+  return await jwtRequest(`/api/v1/notes/${noteId}/shares/${shareId}/`, {
     method: "DELETE",
     headers: { "Content-Type": "application/json" },
   });
-
-  return await checkResponse(response);
 }
--- notes/static/notes/js/notesManager.js
+++ notes/static/notes/js/notesManager.js
@@ -1,146 +1,146 @@
 import { createNote, deleteNote, updateNote, getNoteList, getNoteDetails } from "./notesAPI.js";
 
-
 export class NotesManager {
-    notePanel;
-    notesList;
+  notePanel;
+  notesList;
 
-    #selectedElement = null;
-    #selectedData = null;
+  #selectedElement = null;
+  #selectedData = null;
 
+  titleInput;
+  contentInput;
 
-    titleInput;
-    contentInput;
+  constructor({ notePanel, notesList }) {
+    this.notePanel = notePanel;
+    this.notesList = notesList;
 
-    constructor({ notePanel, notesList }) {
-        this.notePanel = notePanel;
-        this.notesList = notesList;
+    this.titleInput = notePanel.querySelector(".note-panel__title");
+    this.contentInput = notePanel.querySelector("textarea");
 
-        this.titleInput = notePanel.querySelector(".note-panel__title");
-        this.contentInput = notePanel.querySelector("textarea");
-        
-        this.#clearPanel();
+    this.#clearPanel();
+  }
+
+  // ----------------
+  // Private helpers
+  // ----------------
+
+  #highlightNote(noteElement) {
+    noteElement.classList.add("note--selected");
+  }
+
+  #unhighlightNote() {
+    this.#selectedElement.classList.remove("note--selected");
+  }
+
+  #renderNote(data) {
+    this.titleInput.value = data?.title ?? "";
+    this.contentInput.value = data?.content ?? "";
+    this.contentInput.disabled = false;
+    this.notePanel.querySelectorAll("button").forEach((btn) => (btn.disabled = false));
+  }
+
+  #clearPanel() {
+    this.titleInput.value = "";
+    this.contentInput.value = "";
+    this.contentInput.disabled = true;
+    this.notePanel.querySelectorAll("button").forEach((btn) => (btn.disabled = true));
+  }
+
+  #appendNoteToList(id, noteTitle) {
+    const noteTemplate = this.notesList.querySelector("#note-template");
+    const noteClone = noteTemplate.content.cloneNode(true);
+
+    const note = noteClone.querySelector(".note");
+    note.dataset.id = id;
+
+    const title = note.querySelector(".note h3");
+    title.textContent = noteTitle;
+
+    this.notesList.appendChild(noteClone);
+
+    return note;
+  }
+
+  // ----------------
+  // Public API
+  // ----------------
+
+  async select(noteElement) {
+    this.unselect();
+    const response = await getNoteDetails(noteElement.dataset.id);
+    this.#highlightNote(noteElement);
+    this.#selectedElement = noteElement;
+    this.#selectedData = response.data;
+    this.#renderNote(response.data);
+  }
+
+  unselect() {
+    if (!this.#selectedElement) return;
+    this.#unhighlightNote();
+    this.#selectedElement = null;
+    this.#selectedData = null;
+    this.#clearPanel();
+  }
+
+  async delete() {
+    if (!this.#selectedData) return;
+    await deleteNote(this.#selectedData.id);
+    this.#selectedElement.remove();
+    this.unselect();
+  }
+
+  async update() {
+    if (!this.#selectedData) return false;
+    // Get fields current value
+    const title = this.titleInput.value;
+    const content = this.contentInput.value;
+
+    // Build payload with only changed fields
+    const payload = {};
+    if (title !== this.#selectedData.title) payload.title = title;
+    if (content !== this.#selectedData.content) payload.content = content;
+
+    // Return early if nothing changed
+    if (Object.keys(payload).length === 0) {
+      return false;
     }
 
-    // ----------------
-    // Private helpers
-    // ----------------
+    const response = await updateNote(this.#selectedData.id, payload);
+    this.#selectedElement.querySelector(".note-title").textContent = response.data.title;
+    this.#selectedData = response.data;
+  }
 
-    #highlightNote(noteElement) {
-        noteElement.classList.add("note--selected");
-    }
+  async create() {
+    const response = await createNote({
+      title: "Untitled",
+      content: "",
+    });
+    this.unselect();
 
-    #unhighlightNote() {
-        this.#selectedElement.classList.remove("note--selected");
-    }
+    const noteElement = this.#appendNoteToList(response.data.id, response.data.title);
+    this.#highlightNote(noteElement);
+    this.#selectedElement = noteElement;
+    this.#selectedData = response.data;
+    this.#renderNote(response.data);
+  }
 
-    #renderNote(data) {
-        this.titleInput.value = data?.title ?? "";
-        this.contentInput.value = data?.content ?? "";
-        this.notePanel.querySelectorAll("button").forEach(btn => btn.disabled = false);
-    }
+  async loadList(filterParams = "") {
+    this.clearList();
+    const response = await getNoteList(filterParams);
+    response.data.forEach((note) => {
+      this.#appendNoteToList(note.id, note.title);
+    });
+  }
 
-    #clearPanel() {
-        this.titleInput.value = "";
-        this.contentInput.value = "";
-        this.notePanel.querySelectorAll("button").forEach(btn => btn.disabled = true);
-    }
+  clearList() {
+    this.notesList.querySelectorAll(".note").forEach((note) => note.remove());
+  }
 
-    #appendNoteToList(id, noteTitle) {
-        const noteTemplate = this.notesList.querySelector("#note-template");
-        const noteClone = noteTemplate.content.cloneNode(true);
+  getData() {
+    return this.#selectedData;
+  }
 
-        const note = noteClone.querySelector(".note");
-        note.dataset.id = id;
-
-        const title = note.querySelector(".note h3");
-        title.textContent = noteTitle;
-
-        this.notesList.appendChild(noteClone);
-
-        return note;
-    }
-
-    // ----------------
-    // Public API
-    // ----------------
-
-    async select(noteElement) {
-        this.unselect();
-        const noteData = await getNoteDetails(noteElement.dataset.id);
-        this.#highlightNote(noteElement);
-        this.#selectedElement = noteElement;
-        this.#selectedData = noteData;
-        this.#renderNote(noteData);
-    }
-
-    unselect() {
-        if (!this.#selectedElement) return;
-        this.#unhighlightNote();
-        this.#selectedElement = null;
-        this.#selectedData = null;
-        this.#clearPanel();
-    }
-
-    async delete() {
-        if (!this.#selectedData) return;
-        await deleteNote(this.#selectedData.id);
-        this.#selectedElement.remove();
-        this.unselect();
-    }
-
-    async update() {
-        if (!this.#selectedData) return false;
-        // Get fields current value
-        const title = this.titleInput.value;
-        const content = this.contentInput.value;
-
-        // Build payload with only changed fields
-        const payload = {};
-        if (title !== this.#selectedData.title) payload.title = title;
-        if (content !== this.#selectedData.content) payload.content = content;
-
-        // Return early if nothing changed
-        if (Object.keys(payload).length === 0) {
-            return false;
-        }
-
-        const data = await updateNote(this.#selectedData.id, payload);
-        this.#selectedElement.querySelector(".note-title").textContent = data.title;
-        this.#selectedData = data;
-    }
-
-    async create() {
-        const data = await createNote({
-            title: "Untitled",
-            content: ""
-        });
-        this.unselect();
-
-        const noteElement = this.#appendNoteToList(data.id, data.title);
-        this.#highlightNote(noteElement);
-        this.#selectedElement = noteElement;
-        this.#selectedData = data;
-        this.#renderNote(data);
-    }
-
-    async loadList(filterParams = "") {
-        this.clearList();
-        const notes = await getNoteList(filterParams);
-        notes.forEach(note => {
-            this.#appendNoteToList(note.id, note.title);
-        });
-    }
-
-    clearList() {
-        this.notesList.querySelectorAll(".note").forEach(note => note.remove());
-    }
-
-    getData() {
-        return this.#selectedData;
-    }
-
-    getSelectedNote() {
-        return this.#selectedElement;
-    }
+  getSelectedNote() {
+    return this.#selectedElement;
+  }
 }
